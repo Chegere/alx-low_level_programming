9b89d02bedf2eac61405a272b2f9c6183d8ce4d0
Christine Chege <christinechege03@gmail.com>
2022-04-21 11:24:56 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - length of a string
+ * @str: char
+ *
+ *Return: int
+ */
+
+void puts_half(char *str)
+{
+int i, j;
+  
+i = 0;
+while (str[i])
+i++;
+j - 1 / 2;
+if (i % 2)
+j += 1;
+while (j < 1)
+{
+_putchar(str[j]);
+j++;
+}
+_putchar('\n');
+}
