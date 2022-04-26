b708d1c732752d4504508e9cc3ad86730aad0225
Christine Chege <christinechege03@gmail.com>
2022-04-26 05:45:45 +0300

    Encodes a string

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,19 @@ char *rot13(char *s)
 {
 int x, y;
 
-  char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
   
-  for (x = 0; s[x] != '\0'; y++)
-  {
-    for (y = 0; input[y] != '\0'; y++)
-    {
-      if s[x] == input[y])
-      {
-        s[x] = output[y];
-        break;
-      }
-    }
-  }
-  return (s);
+for (x = 0; s[x] != '\0'; y++)
+{
+for (y = 0; input[y] != '\0'; y++)
+{
+if (s[x] == input[y])
+{
+s[x] = output[y];
+break;
+}
+}
+}
+return (s);
 )
