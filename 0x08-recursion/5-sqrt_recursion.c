f100347ce64b89684de42a0ad6ec3df5a43273c9
Christine Chege <christinechege03@gmail.com>
2022-04-29 10:24:53 -0700

    Square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,19 +3,39 @@
  * _evaluate - evaluate function sqrt
  * @i: integer
  * @n: integer
- * Return: evaluate sqrt
+ * Return: evaluate function sqrt
  */
 
 int _evaluate(int i, int n)
 {
-/*Evaluate function*/
 if (n == 0 || n == 1)
 return (n);
 
 else if (i * i < n)
+return (_evaluate(i + 1, n));
+
+else if (i * i == n)
 return (i);
 
 return (-1);
 
 return (-1);
+}
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integer
+ * Return: sqrt_recursion
+ */
+
+int _sqrt_recursion(int n)
+{
+int i = 0;
+
+if (i < 0)
+return (-1);
+
+else
+{
+return (_evaluate(i, n));
+}
 }
